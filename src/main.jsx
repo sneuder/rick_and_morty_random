fab67a0b6c57f21dfd107813a6d79683583576fe
Esneider Ospina <sneuder8@gmail.com>
2022-05-07 20:25:19 -0500

    Establishing client

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ApolloProvider } from "@apollo/client";
+import client from "./graphql/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
-)
+);
