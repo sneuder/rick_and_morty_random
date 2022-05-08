c32310277555f3d1ceb2e1c0e4052ce15643260e
Esneider Ospina <sneuder8@gmail.com>
2022-05-08 08:35:13 -0500

    fix style bug

--- src/shared/Text.js
+++ src/shared/Text.js
@@ -3,9 +3,14 @@ import styled from "styled-components";
 export const Text = styled.p`
   ${props => props.attribute && `
     font-weight: 700;
+    padding-right: 1rem;
     &::first-letter {
       text-transform: uppercase;
     }
   `}
-  ${props => props.value && `font-weight: 400`}
+  ${props => props.value && `
+    font-weight: 400;
+    width: 70%;
+    text-align: right;
+  `}
 `;
