8d26fd90e1f61c58ae1ad23aeea350808290b3ae
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-12-24 17:20:23 -0500

    custom hooks for characters and context

---
+++ src/hooks/useSetContext.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const useSetContext = () => {
+  const [characters, setCharacters] = useState({
+    main: undefined,
+    history: [],
+  });
+
+  const setMainCharacter = (newCharacter) => {
+    setCharacters((prev) => ({
+      ...prev,
+      main: undefined,
+    }));
+
+    setCharacters((prev) => ({
+      ...prev,
+      main: newCharacter,
+    }));
+  };
+
+  const updateHistory = () => {
+    if (!characters.main) return;
+    setCharacters({
+      ...characters,
+      history: [characters.main, ...characters.history],
+    });
+  };
+
+  return {
+    characters,
+    setMainCharacter,
+    updateHistory,
+  };
+};
+
+export default useSetContext;
